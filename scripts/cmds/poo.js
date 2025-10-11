6b9653a9663709d2e6b5f82bcef8de29d7bf554f
jahidulislamsagor10-debug <jahidulislamsagor10@gmail.com>
2025-10-12 01:05:02 +0600

    Update poo.js

--- scripts/cmds/poo.js
+++ scripts/cmds/poo.js
@@ -5,9 +5,9 @@ module.exports = {
     role: 0,
     author: "SaGor",
     description: "Send a meme with a tagged person stepping in poo",
-    commandCategory: "meme",
-    usages: "[tag]",
-    cooldowns: 5,
+    category: "meme",
+    usages: "[tag someone]",
+    cooldowns: 5
   },
 
   onStart: async function ({ api, event, args }) {
@@ -17,11 +17,10 @@ module.exports = {
     const jimp = require("jimp");
     const request = require("request");
 
-    const __root = path.resolve(__dirname, "cache", "canvas");
-    const dirMaterial = __root + "/";
-    if (!fs.existsSync(dirMaterial)) fs.mkdirSync(dirMaterial, { recursive: true });
+    const __root = path.resolve(__dirname, "cache/canvas");
+    if (!fs.existsSync(__root)) fs.mkdirSync(__root, { recursive: true });
 
-    const baseImagePath = dirMaterial + "poo.png";
+    const baseImagePath = path.join(__root, "poo.png");
     if (!fs.existsSync(baseImagePath)) {
       await new Promise(resolve => {
         request("https://i.imgur.com/5qT35LF.png").pipe(fs.createWriteStream(baseImagePath)).on("close", resolve);
@@ -32,34 +31,31 @@ module.exports = {
     if (!mention) return api.sendMessage("❌ Please tag someone!", event.threadID, event.messageID);
 
     const makeImage = async (userID) => {
-      let point_image = await jimp.read(baseImagePath);
-      let pathImg = dirMaterial + `/point_${userID}.png`;
-      let avatarPath = dirMaterial + `/avt_${userID}.png`;
-
-      let avatarData = (await axios.get(`https://graph.facebook.com/${userID}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
+      const base = await jimp.read(baseImagePath);
+      const avatarPath = path.join(__root, `avt_${userID}.png`);
+      const avatarData = (await axios.get(`https://graph.facebook.com/${userID}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
       fs.writeFileSync(avatarPath, Buffer.from(avatarData, 'utf-8'));
 
-      let circleAvatar = await jimp.read(await (async img => {
+      const avatarCircle = await jimp.read(await (async img => {
         let image = await jimp.read(img);
         image.circle();
         return await image.getBufferAsync("image/png");
       })(avatarPath));
 
-      point_image.composite(circleAvatar.resize(100, 100), 390, 680);
+      base.composite(avatarCircle.resize(100, 100), 390, 680);
 
-      let buffer = await point_image.getBufferAsync("image/png");
-      fs.writeFileSync(pathImg, buffer);
+      const outPath = path.join(__root, `poo_${userID}.png`);
+      await base.writeAsync(outPath);
       fs.unlinkSync(avatarPath);
-
-      return pathImg;
+      return outPath;
     };
 
-    const pathImg = await makeImage(mention);
+    const imagePath = await makeImage(mention);
     const nameTag = event.mentions[mention].replace("@", "");
 
     return api.sendMessage({
       body: `${nameTag} Ew, I stepped in a shit 💩`,
-      attachment: fs.createReadStream(pathImg)
-    }, event.threadID, () => fs.unlinkSync(pathImg), event.messageID);
+      attachment: fs.createReadStream(imagePath)
+    }, event.threadID, () => fs.unlinkSync(imagePath), event.messageID);
   }
 };
